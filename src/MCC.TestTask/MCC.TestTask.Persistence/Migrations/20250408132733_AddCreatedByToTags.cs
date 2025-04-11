198a375d2d29a909e939cee526b0fb420dd9cdba
Grinch <grinch@ruin.xmas.com>
2025-04-12 04:10:32 +0700

    Re-add <file-name> after revert

---
+++ src/MCC.TestTask/MCC.TestTask.Persistence/Migrations/20250408132733_AddCreatedByToTags.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MCC.TestTask.Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedByToTags : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedById",
+                table: "Tags",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedById",
+                table: "Tags");
+        }
+    }
+}
